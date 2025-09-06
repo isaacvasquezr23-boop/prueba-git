29f82d0bec04b58ea31acb59d27e821e59b55c28
Isaac David Vasquez Ramos <ivasquezr@unicartagena.edu.co>
2025-09-05 23:02:42 -0500

    feat(practica): añade programa Hola Mundo en Java

---
+++ src/poo/practica/Main.java
@@ -0,0 +1,7 @@
+package poo.practica;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Isaac, este es un ejercicio de práctica con Java y GitHub!");
+    }
+}
